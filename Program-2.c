6a5ff5a1f04862bb06d6131693004b77d2dee505
fardeen857 <20me0037@vvce.ac.in>
2021-04-01 19:45:40 +0000

    2

--- Program-2.c
+++ Program-2.c
@@ -1,4 +1,4 @@
-t//Fill in the missing code in the below sample code which copies a given string into another string.
+//Fill in the missing code in the below sample code which copies a given string into another string.
 // Instructions:-
 //1) Initially, read a string from the standard input device and write a loop to copy each character of given string into another string till the end of the string is reached.
 //2) Place '\0' at the end of the copied string.
@@ -13,13 +13,13 @@ int main()
 {
 	char str1[50], str2[50];
 	int i;
-	printf("Enter a string-1: ");
+	printf("Enter a String-1: ");
 	scanf("%s",str1);
 	for (i=0;i<50;i++)                  //Complete the code in for-loop & copy logic
   {                          
-		str2[i]= str1[i];
+		str2[i]=str1[i];
 	}
-	str2[i]= str1[50] ;                  //Complete the statement
+	str2[i]=str1[50];                  //Complete the statement
 	printf("The copied String-2 is %s",str2);
 	return 0;
 }
